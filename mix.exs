6b77218e507f55a4ad8db112cd74314757c96b5c
Jacek Adamek <jacek.adamek@gmail.com>
2016-05-10 01:05:23 +0200

    Some metadata updates in the mix file

--- mix.exs
+++ mix.exs
@@ -7,27 +7,31 @@ defmodule PhoenixReactor.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     package: package,
+     description: description]
   end
 
-  # Configuration for the OTP application
-  #
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:phoenix_html, "~> 2.5"},
     {:poison, "~> 1.5.2"}]
+  end
+
+  defp package do
+    [contributors: ["Jacek Adamek"],
+    licenses: ["MIT"],
+    links: %{ "Github" => "https://github.com/jacek-adamek/phoenix_reactor" }]
+  end
+
+  defp description do
+    """
+    Library which provides an easy way to use React and Phoenix framework.
+    """
   end
 end
